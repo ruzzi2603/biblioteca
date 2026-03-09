672951b87db5a30a315b72cd8abad854fb3f65ed
Matheus Ruzzi Ribeiro <matheusruzzi8@gmail.com>
2026-03-09 16:11:01 -0300

    Add files via upload

---
+++ biblioteca/config/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import (
+    IndexView,
+    LivrosView,
+    EmprestimoView,
+    CidadesView,
+    AutoresView,
+    EditorasView,
+    LeitoresView,
+    GenerosView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('livros/', LivrosView.as_view(), name='livros'),
+    path('reserva/', EmprestimoView.as_view(), name='reserva'),
+    path('cidade/', CidadesView.as_view(), name='cidade'),
+    path('autor/', AutoresView.as_view(), name='autor'),
+    path('editora/', EditorasView.as_view(), name='editora'),
+    path('leitor/', LeitoresView.as_view(), name='leitor'),
+    path('genero/', GenerosView.as_view(), name='genero'),
+]
